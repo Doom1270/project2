9f279f9ae106f96f4c9dfc990d17496459f6edf8
Doom1270 <115984839+Doom1270@users.noreply.github.com>
2022-12-02 16:10:22 +0000

    Add files via upload

---
+++ controllers/game.js
@@ -0,0 +1,65 @@
+
+const pool = require("../data/config");
+
+const getAllgames = (request, response) => {
+    pool.query(` SELECT * FROM game `, (error, result) => {
+
+        if (error){
+            throw error;
+        }
+
+        console.log(result);
+
+
+        response.render("../views/pages/games", {
+            title: "games",
+            gamesArray: result
+        });
+    });
+};
+const getgameById = (request, response, next) => {
+    const id = request.params.id;
+    pool.query(`SELECT * FROM game WHERE game_id = ?`, 
+  id, (error, result) => {
+            if (error){
+                throw error;
+            }
+ 
+            console.log(result);
+            response.render("../views/pages/game", {
+                game: result[0],
+                title: "game"
+            });
+        });
+};
+const addgame = (request, response, next) => {
+    console.log(request.body);
+    pool.query("INSERT INTO game SET ?", request.body, (error, result) => {
+        if (error){
+            throw error;
+        }
+        response.redirect(`/game`);
+    });
+};
+
+const deletegame = (request, response, next) => {
+    const id = request.params.id;
+ 
+    pool.query(`DELETE FROM game WHERE game_id = ?`, 
+  id, (error, result) => {
+        if (error){
+            throw error;
+        }
+        console.log(request.body);
+        response.redirect(`/game`);
+    });
+};
+
+
+
+module.exports = {
+    getAllgames,
+    getgameById,
+    addgame,
+    deletegame,
+};
